9445158b34022c945155ef6da1f296e188bbbe95
natferlima <ferlima.natalia@gmail.com>
2022-06-24 19:37:04 -0300

    feat: cria a função scrape_next_page_link

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -25,7 +25,9 @@ def scrape_novidades(html_content):
 
 # Requisito 3
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    result = selector.css("div.nav-links > a.next::attr(href)").get()
+    return result
 
 
 # Requisito 4
