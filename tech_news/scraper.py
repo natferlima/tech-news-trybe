106b66e21b1dc0ec72078b595aa0677b111cc6a3
natferlima <ferlima.natalia@gmail.com>
2022-06-24 19:28:00 -0300

    feat: cria função scrape_novidades

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,5 +1,6 @@
 import requests
 import time
+from parsel import Selector
 
 
 # Requisito 1
@@ -17,7 +18,9 @@ def fetch(url):
 
 # Requisito 2
 def scrape_novidades(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    result = selector.css("h2.entry-title > a::attr(href)").getall()
+    return result
 
 
 # Requisito 3
